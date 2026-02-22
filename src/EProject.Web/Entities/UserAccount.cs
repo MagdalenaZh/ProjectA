c9acf9112321f24c380f0e20dda59ef42b99a9d0
Arina Agayeva <a.agayeva.pavlodar@gmail.com>
2026-02-22 15:46:32 +0200

    Implement entity & validation

---
+++ src/EProject.Web/Entities/UserAccount.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+namespace EProject.Web.Entities
+{
+    [Index(nameof(Email), IsUnique = true)]
+    public class UserAccount
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        [MaxLength(100, ErrorMessage = "Email cannot exceed 100 characters.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*\.)[A-Za-z\d\.]{8,}$",
+        ErrorMessage = "Password must be at least 8 characters long and contain letters, numbers, and at least one dot.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [MaxLength(50, ErrorMessage = "Name should be less than 50 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Phone is required.")]
+        [Phone(ErrorMessage = "Invalid phone number format.")]
+        public string Phone { get; set; }
+
+    }
+}
